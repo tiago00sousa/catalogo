c429aaed8aff16cde01449457cf98260aa75f0f1
Tiago Aguiar <firewall.hidden@hotmail.com>
2023-01-27 20:38:52 -0300

    criado arquivos de autoload com composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '8e83b78729da1454daed5d2c4241c043c600c10e',
+        'name' => 'catalogo/digital',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'catalogo/digital' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '8e83b78729da1454daed5d2c4241c043c600c10e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
